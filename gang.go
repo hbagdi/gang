95e0bfe42845c6a7838086ccdf9aecdd4524833b
Harry Bagdi <harrybagdi@gmail.com>
2020-05-17 21:56:00 -0700

    add godoc comments

--- gang.go
+++ gang.go
@@ -5,6 +5,7 @@ import (
 	"sync"
 )
 
+// Package gang provides utilities to manage multiple goroutines.
 type Gang struct {
 	ContinueOnErrExit   bool
 	ContinueOnCleanExit bool
@@ -16,12 +17,14 @@ type Gang struct {
 	once  sync.Once
 }
 
+// withLock runs fn under fnMutex lock.
 func (g *Gang) withLock(fn func()) {
 	g.fnMutex.Lock()
 	defer g.fnMutex.Unlock()
 	fn()
 }
 
+// AddWithChan adds fn to Gang. fn will be run when Run() is invoked.
 func (g *Gang) AddWithChan(fn func(<-chan struct{})) {
 	g.withLock(func() {
 		g.fns = append(g.fns, func(ctx context.Context) error {
@@ -31,6 +34,7 @@ func (g *Gang) AddWithChan(fn func(<-chan struct{})) {
 	})
 }
 
+// AddWithCtx adds fn to Gang. fn will be run when Run() is invoked.
 func (g *Gang) AddWithCtx(fn func(context.Context)) {
 	g.withLock(func() {
 		g.fns = append(g.fns, func(ctx context.Context) error {
@@ -40,6 +44,7 @@ func (g *Gang) AddWithCtx(fn func(context.Context)) {
 	})
 }
 
+// AddWithChanE adds fn to Gang. fn will be run when Run() is invoked.
 func (g *Gang) AddWithChanE(fn func(<-chan struct{}) error) {
 	g.withLock(func() {
 		g.fns = append(g.fns, func(ctx context.Context) error {
@@ -48,6 +53,7 @@ func (g *Gang) AddWithChanE(fn func(<-chan struct{}) error) {
 	})
 }
 
+// AddWithCtxE adds fn to Gang. fn will be run when Run() is invoked.
 func (g *Gang) AddWithCtxE(fn func(context.Context) error) {
 	g.withLock(func() {
 		g.fns = append(g.fns, func(ctx context.Context) error {
@@ -56,6 +62,14 @@ func (g *Gang) AddWithCtxE(fn func(context.Context) error) {
 	})
 }
 
+// Run executes all functions that have been added to the
+// Gang via Add* functions. Each function executes in it's own goroutines.
+// If the provided context is cancelled, context (or done) channel passed to
+// added function is cancelled (or closed) to cleanly terminate all functions.
+// If any of the function errors out or exits, then the context
+// (or channel) passed to added function is cancelled (or closed) as well.
+// The returned channel returns errors (if any) returned by all of
+// added functions and is closed once all the functions have finished executing.
 func (g *Gang) Run(ctx context.Context) <-chan error {
 	g.once.Do(func() {
 		var wg sync.WaitGroup
